e78bac207a72ad399e790082ab3e931d5299b4cd
Kamesh Chathuranga <kameshchathuranga7@gmail.com>
2025-03-31 19:00:32 +0530

    update prisma schema

--- src/controller/MessageController.ts
+++ src/controller/MessageController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { prisma } from "../utils/PrismaClient";
 import { onlineUsers } from "..";
 
 const createMessage = async (req: Request, res: Response) => {
@@ -11,19 +10,19 @@ const createMessage = async (req: Request, res: Response) => {
     }
 
     const isOnlineUser = onlineUsers.get(to);
-    const newMessage = await prisma.message.create({
-      data: {
-        message,
-        author: { connect: { id: from } },
-        receiver: { connect: { id: to } },
-        status: isOnlineUser ? "delivered" : "sent",
-      },
-      include: {
-        author: true,
-        receiver: true,
-      },
-    });
-    return res.status(201).json(newMessage);
+    // const newMessage = await prisma.message.create({
+    //   data: {
+    //     message,
+    //     author: { connect: { id: from } },
+    //     receiver: { connect: { id: to } },
+    //     status: isOnlineUser ? "delivered" : "sent",
+    //   },
+    //   include: {
+    //     author: true,
+    //     receiver: true,
+    //   },
+    // });
+    // return res.status(201).json(newMessage);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to send message" });
@@ -33,40 +32,40 @@ const createMessage = async (req: Request, res: Response) => {
 const getAllUserMessages = async (req: Request, res: Response) => {
   try {
     const { from, to } = req.params;
-    const messages = await prisma.message.findMany({
-      where: {
-        OR: [
-          {
-            authorId: from,
-            receiverId: to,
-          },
-          {
-            authorId: to,
-            receiverId: from,
-          },
-        ],
-      },
-      orderBy: {
-        id: "asc",
-      },
-    });
+    // const messages = await prisma.message.findMany({
+    //   where: {
+    //     OR: [
+    //       {
+    //         authorId: from,
+    //         receiverId: to,
+    //       },
+    //       {
+    //         authorId: to,
+    //         receiverId: from,
+    //       },
+    //     ],
+    //   },
+    //   orderBy: {
+    //     id: "asc",
+    //   },
+    // });
 
-    const unreadMessages = messages
-      .filter((message) => message.status !== "read" && message.authorId === to)
-      .map((message) => message.id);
+    // const unreadMessages = messages
+    //   .filter((message) => message.status !== "read" && message.authorId === to)
+    //   .map((message) => message.id);
 
-    await prisma.message.updateMany({
-      where: {
-        id: {
-          in: unreadMessages,
-        },
-      },
-      data: {
-        status: "read",
-      },
-    });
+    // await prisma.message.updateMany({
+    //   where: {
+    //     id: {
+    //       in: unreadMessages,
+    //     },
+    //   },
+    //   data: {
+    //     status: "read",
+    //   },
+    // });
 
-    return res.status(200).json(messages);
+    // return res.status(200).json(messages);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to get messages" });
